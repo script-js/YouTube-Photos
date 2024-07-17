a8ef0eaa2759a82b366073b30243b3ebc3763167
script-js <134544514+script-js@users.noreply.github.com>
2024-07-17 12:07:36 -0700

    Create googleauth.js

---
+++ googleauth.js
@@ -0,0 +1,44 @@
+const CLIENT_ID = '1042189889229-ecp73b2drihjcm65930381l07m3qm24a.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAdAjrwSHXXNIrq5XqcI6NACfO8UDVwjfY';
+var FNAME = "syncjsdata.json"
+
+// Discovery URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+
+// Set API access scope before proceeding authorization request
+const SCOPES = 'https://www.googleapis.com/auth/drive.file \ https://www.googleapis.com/auth/userinfo.profile';
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+function gapiLoaded() {
+   gapi.load('client', initializeGapiClient);
+}
+
+async function initializeGapiClient() {
+   await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+   });
+   gapiInited = true;
+   var token = localStorage.getItem('gapi_token');
+   if (token) {
+      gapi.client.setToken(JSON.parse(token));
+   } else if (!location.href.includes("login")) {
+      location.replace("login?handoff=" + btoa(location.href).replaceAll("+","%2B"))
+   }
+   if (!location.href.includes("login")) {setInterval(expireCheck,500)}
+   startup()
+}
+
+
+function gisLoaded() {
+   tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
+      access_type: 'offline',
+      expires_in: 43200,
+      callback: ''
+   });
+   gisInited = true;
+}
