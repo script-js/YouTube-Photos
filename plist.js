2240655189696761538bad11dd1b2fbf4b839d5e
script-js <134544514+script-js@users.noreply.github.com>
2024-07-21 12:36:21 -0700

    Update plist.js

--- plist.js
+++ plist.js
@@ -1,48 +1,7 @@
-function getChannel() {
+var playlist;
+
+function getPlaylist() {
    gapi.client.request({
-    path: '/youtube/v3/channels',
-    params: {
-      part: 'snippet',
-      mine: true
-    },
-    callback: function(response) {
-      if (response.error) {
-        console.log(response.error.message);
-      } else {
-        return response.items[0].id
-      }
-    }.bind(this)
-  });
-}
-
-async function getPlaylists() {
-    const channelId = 'YOUR_CHANNEL_ID';
-
-    try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=${channelId}&key=${API_KEY}`);
-        const data = await response.json();
-
-        // Extract relevant information from the API response
-        const playlists = data.items.map(item => ({
-            id: item.id,
-            title: item.snippet.title,
-            description: item.snippet.description,
-            thumbnailUrl: item.snippet.thumbnails.default.url,
-        }));
-
-        // Create a JSON variable with the playlists
-        const playlistsJSON = JSON.stringify(playlists, null, 2);
-        console.log(playlistsJSON); // You can replace this with any other action you want
-
-        return playlistsJSON;
-    } catch (error) {
-        console.error('Error fetching playlists:', error);
-        return null;
-    }
-}
-
-function getPlaylists2() {
-   this.gapi.client.request({
     path: '/youtube/v3/playlists',
     params: {
       part: 'snippet',
@@ -52,7 +11,14 @@ function getPlaylists2() {
       if (response.error) {
         console.log(response.error.message);
       } else {
-        console.log(response)
+        if (response.kind == "youtube#playlistListResponse") {
+          var playlist = response.items.forEach(function(k) {
+             if (k.snippet.title == "YouTubePhotosLibrary") {
+                return k;
+             }
+          })
+         console.log(playlist)
+        }
       }
     }.bind(this)
   });
