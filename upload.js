f8379d92dd8959a937e105ffbb536ba8fbe009bc
script-js <134544514+script-js@users.noreply.github.com>
2024-07-25 08:47:03 -0600

    Update upload.js

--- upload.js
+++ upload.js
@@ -46,7 +46,7 @@
             };
 
             recorder.onstop = function () {
-                    console.log(chunks[0])
+                    console.log(chunks)
               if (chunks[0].size > 217) {
                 var blob = new Blob(chunks,
                                       { type: 'video/webm' })
