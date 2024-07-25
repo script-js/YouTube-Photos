b6e94f64329745e2fcb69b192af1b8fa9faae01a
script-js <134544514+script-js@users.noreply.github.com>
2024-07-25 17:50:34 -0600

    Update upload.js

--- upload.js
+++ upload.js
@@ -57,10 +57,6 @@
                return;
             },1000)
             const drawFrame = () => {
-                    if (currentImageIndex >= images.length) {
-                      recorder.stop();
-                      return;
-                    }
                     ctx.drawImage(image, 0, 0,
                                   canvas.width, canvas.height);
             };
