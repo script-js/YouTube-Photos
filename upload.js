a58dc498e696834840349ed6d3f65baa268b6953
script-js <134544514+script-js@users.noreply.github.com>
2024-07-26 12:25:26 -0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -96,7 +96,7 @@ function showLink(obj,meta,title) {
   var url = URL.createObjectURL(obj)
   var link = document.createElement("li")
   link.innerHTML = `
-    <p><a href="${url}" download="${title}">Download</a></p>
+    <p><a href="${url}" download="${title}">${title}</a></p>
     <input style="margin-right:20px" placeholder="Video ID">
   `
   var newbtn = document.createElement("button")
